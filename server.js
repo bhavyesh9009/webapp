1f9d593a5c60bf26dcc996a7bbc3fe64b083e7f5
bhavyesh9009 <pa.bh9009@gmail.com>
2021-03-25 22:49:29 +0530

    D:/Git/new prefix added

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res) {
-    res.send("IT WORKED")
+    res.send("IT WORKED!!!!")
+})
+
+app.get("/new", function(req,res) {
+    res.send("New Also Worked!!!!")
 })
 
 app.listen(process.env.PORT || 5000)
